73d20d3dd05b1262cd718e67a9761b14bedef2b5
YashParakh5 <yashparakh5@gmail.com>
2021-08-02 10:57:47 +0530

    fixes

--- backend/routers/prodRouter.js
+++ backend/routers/prodRouter.js
@@ -5,12 +5,17 @@ import Product from '../models/prodModel.js';
 
 const prodRouter = express.Router();
 
-prodRouter.get('/', expressAsyncHandler((req,res) =>{
+prodRouter.get('/', expressAsyncHandler(async(req,res) =>{
     const products = await Product.find({});
     res.send({ products }); 
 }));
 
-prodRouter.get('/seed', expressAsyncHandler((req,res) =>{
+prodRouter.get('/category', expressAsyncHandler(async(req, res) =>{
+      const category = await Product.find().distinct('category');
+      res.send(category);
+}));
+
+prodRouter.get('/seed', expressAsyncHandler(async(req,res) =>{
     const createdProducts = await Product.insertMany(data.products);
     res.send({ createdProducts });
 }));
